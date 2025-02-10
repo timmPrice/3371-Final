9adb4201a9262cb37d8a96cdbc31652f81fd3e70
timmPrice <timmPrice6@gmail.com>
2025-02-10 14:42:40 -0500

    finished rough c# code for postfix evaluation

---
+++ postfix-evaluation-cs/Postfix.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+class PostfixEvaluation {
+    static int EvaluatePostfix(string expression) {
+        Stack<int> stack = new Stack<int>();
+        foreach (char c in expression) {
+            if(char.IsDigit(c)) {
+                stack.Push(c - '0'); // char -> int -> push to stack
+            } else if (c == ' '){
+                continue; 
+            } else {
+                int a = stack.Pop();
+                int b = stack.Pop();
+                switch(c) {
+                    case '+':
+                        stack.Push(a + b);
+                        break;
+                    case '-':
+                        stack.Push(b - a);
+                        break;
+                    case '*':
+                        stack.Push(a * b);
+                        break;
+                    case '/':
+                        stack.Push(b / a);
+                        break;
+                }
+            }
+        }
+        return stack.Pop();
+    }
+    static void Main(){
+        string expression = "231*+9 - ";
+        Console.WriteLine("Postfix Evaluation : " + EvaluatePostfix(expression));
+    }
+}
